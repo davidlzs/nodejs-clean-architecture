ef2c2b8457e477eeb8efbe309eb3ab4f19a98798
David L <davidlzs@hotmail.com>
2018-02-28 10:11:31 -0600

    stage 2: layerd solution: extract DB layer

---
+++ db/Users.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/CleanArchitecture");
+
+const User = mongoose.model("Users", {
+  fullname: String,
+  firstname: String,
+  lastname: String,
+  age: Number
+});
+
+const saveUser = (userData) => {
+  return new Promise((resolve, reject) => {
+    var user = new User(userData);
+    user.save()
+      .then(user => {
+        console.log('User saved', user);
+        resolve(user);
+      })
+      .catch(err => {
+        console.log('Unable to save user', err);
+        reject(user);
+      });
+  });
+}
+
+module.exports = {
+  saveUser
+}
--- server.js
+++ server.js
@@ -2,34 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
-
+const Users = require('./db/Users');
 
 mongoose.connect("mongodb://localhost:27017/CleanArchitecture");
-
-const User = mongoose.model("Users", {
-  fullname: String,
-  firstname: String,
-  lastname: String,
-  age: Number
-});
 
 var app = new express();
 app.use(bodyParser.json());
 
 app.post('/users', (req, res) => {
   var body = req.body;
-  var user = _.pick(body,  ['firstname', 'lastname', 'age']);
-  var dbUser = new User(user);
-  dbUser.save().then((result) => {
-    console.log('User saved', result);
+  var userData = _.pick(body,  ['firstname', 'lastname', 'age']);
+
+  Users.saveUser(userData).then((result) => {
     res.status(200).send({
       status: 'success',
       result
     });
   }).catch((err) => {
-    console.log('Unable to save user', err);
     res.status(400).send({status: 'failure'});
-  })
+  });
 });
 
 app.listen(3000, () => {
